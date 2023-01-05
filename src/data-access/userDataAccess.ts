5b885cc9afc1560358913aab924de00505f18bd9
Hrant Grishyan <grishyan.hrant@gmail.com>
2023-01-05 17:52:39 +0400

    fix: delete db url

--- src/data-access/userDataAccess.ts
+++ src/data-access/userDataAccess.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv';
 import { Sequelize } from 'sequelize';
 
-export const UserSequelize = new Sequelize('postgres://koyxfdht:dYwk7Iqe9qz9lH_vPHUucKBxKgHqOi20@mel.db.elephantsql.com/koyxfdht');
+dotenv.config();
+
+export const UserSequelize = new Sequelize(process.env.DB_URL);
