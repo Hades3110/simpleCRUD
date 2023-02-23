8a5e6b08b4717e9fe31b4067c694cc5e5dd49aa5
Hrant Grishyan <grishyan.hrant@gmail.com>
2023-02-23 16:59:28 +0400

    feat: added JWT auth

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { usersRoutes, groupsRouter } from './routes';
+import cors from 'cors';
+import { usersRoutes, groupsRouter, loginRouter } from './routes';
 import { log } from './middleware/logger';
+import { authMiddleware } from './middleware/auth';
 import * as process from 'process';
 
 dotenv.config();
@@ -11,6 +13,16 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+const corsOptions = {
+    origin: '*',
+    allowedHeaders: ['Content-Type', 'authorization'],
+};
+
+app.use(cors(corsOptions));
+
+app.use('/api/login', loginRouter);
+
+app.use(authMiddleware);
 app.use('/api/users', usersRoutes);
 app.use('/api/groups', groupsRouter);
 app.use(log);
