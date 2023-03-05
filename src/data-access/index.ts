fc5e497f33586cfc708ebd300d1b9caa9b61fb45
Hrant Grishyan <grishyan.hrant@gmail.com>
2023-03-05 16:12:40 +0400

    feat: added dialect

--- src/data-access/index.ts
+++ src/data-access/index.ts
@@ -3,4 +3,6 @@ import { Sequelize } from 'sequelize';
 
 dotenv.config();
 
-export const sequelize = new Sequelize(process.env.DB_URL);
+export const sequelize = new Sequelize(process.env.DB_URL, {
+    dialect: 'postgres',
+});
